8cf55a60e91057dac68973090c8adeb0c548fa5a
Raudonkepuris <54080687+Raudonkepuris@users.noreply.github.com>
2020-06-24 11:15:09 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+clear
+sudo cp hentaitopdf /usr/bin/hentaitopdf
+sudo chmod +x /usr/bin/hentaitopdf
+clear
+echo "Beat covid-19, by beating your meat
+
+Script for turning downloaded hentai from folders of pictures into one pdf file
+by Raudonkepuris
+
+Begin using by writing
+hentaitopodf (relative or absolute path) (name for the generated pdf file)
+pdf file will be saved into the same folder you gave path to"
